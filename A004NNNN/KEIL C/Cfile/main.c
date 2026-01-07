6c2f11d1cf9d48953343c091a367172c0b6d9a13
kErrb3rt <laoye2408171283@outlook.com>
2026-01-07 21:46:50 +0800

    normla

---
+++ A004NNNN/KEIL C/Cfile/main.c
@@ -0,0 +1,104 @@
+#include <reg51.h>
+#include <intrins.h>  // ?? _nop_(),????????
+
+// ??????(?????,???????????)
+unsigned char status = 1;  // ??? var09(???)
+unsigned char counter = 0; // ??? var08(????)
+
+// ????(16??? + ???,???????)
+void delay(unsigned char high, unsigned char low)
+{
+    unsigned char h = high;
+    unsigned char l = low;
+    unsigned char i;
+    
+    while (h || l)
+    {
+        if (l == 0)
+        {
+            if (h == 0) break;  // ????
+            h--;
+            l = 0xFF;
+        }
+        else
+        {
+            l--;
+        }
+        
+        // ?????? 250 ?,???? _nop_() ?? Keil ?????
+        // ?? 12MHz ??,?????? 0.3~0.5s,???? 3~5s(????)
+        for (i = 0; i < 250; i++)
+        {
+            _nop_(); _nop_(); _nop_(); _nop_(); _nop_();
+            _nop_(); _nop_(); _nop_(); _nop_(); _nop_();
+        }
+    }
+}
+
+void main(void)
+{
+    SP = 0x09;  // ??????
+    
+    status = 1;
+    counter = 0;
+    
+    while (1)
+    {
+        switch (status)
+        {
+            case 1:  // ????:???? + ???(????????)
+                P0 = 0x33;  // 00110011 ? P0.0=1, P0.1=1, P0.2=0, P0.3=0, P0.4=1, P0.5=1
+                // ???? LED ??????(???),??:P0 = ~0x33;  // ? 11001100
+                delay(0xB8, 0x0B);
+                status = 2;
+                break;
+                
+            case 2:  // ???? 12 ?(??? 0.4s ????,? 6 ????????)
+                if (counter < 12)
+                {
+                    P0 ^= 0x02;        // ?? P0.1(????)
+                    delay(0x2C, 0x01); // ???(?????,????)
+                    counter++;
+                    if (counter == 6)
+                    {
+                        P0 |= 0x04;    // ? 6 ???? P0.2
+                    }
+                }
+                else
+                {
+                    counter = 0;
+                    status = 3;        // ????,???????
+                }
+                break;
+                
+            case 3:  // ????:???????? + ???
+                P0 = 0x3A;  // 00111010 ? P0.0=0, P0.1=1, P0.2=1, P0.3=1, P0.4=1, P0.5=0
+                // ???????,??:P0 = ~0x3A;  // ? 11000101
+                delay(0xD0, 0x07);
+                status = 4;
+                break;
+                
+            case 4:  // ?????? 12 ?
+                if (counter < 12)
+                {
+                    P0 ^= 0x10;        // ?? P0.4
+                    delay(0x2C, 0x01);
+                    counter++;
+                    if (counter == 6)
+                    {
+                        P0 |= 0x20;    // ? 6 ???? P0.5
+                    }
+                }
+                else
+                {
+                    counter = 0;
+                    status = 1;        // ??,???????
+                }
+                break;
+                
+            default:
+                status = 1;  // ????
+                break;
+        }
+    }
+}
